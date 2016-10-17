eac1f8a716d77f3e04a874250dcfb2ac86ab4592
xrf416933696 <xrf930301@163.com>
2016-10-17 15:02:24 +0800

    test

---
+++ src/com/bupt/termdemo/controller/LoginController.java
@@ -0,0 +1,30 @@
+package com.bupt.termdemo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bupt.termdemo.dao.IUserDao;
+import com.bupt.termdemo.model.User;
+
+@Controller
+@RequestMapping("/loginCtl")
+public class LoginController {
+
+	@Autowired
+	private IUserDao userDao;
+	
+	@RequestMapping("/login")
+	public String login(User user){
+		System.out.println(user.getUsername());
+		System.out.println(user.getPassword());
+
+		System.out.println();
+		
+		User loginuser = userDao.login(user);
+		System.out.println(loginuser.getUserrole());
+		
+		return "index";
+	}
+	
+}
