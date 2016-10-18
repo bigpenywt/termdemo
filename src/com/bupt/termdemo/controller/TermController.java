349588273063c2dd328fb849972c62920d47a52e
xrf416933696 <xrf930301@163.com>
2016-10-18 16:22:43 +0800

    TERM CONTROLLER

---
+++ src/com/bupt/termdemo/controller/TermController.java
@@ -0,0 +1,10 @@
+package com.bupt.termdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/loginCtl")
+public class TermController {
+
+}
