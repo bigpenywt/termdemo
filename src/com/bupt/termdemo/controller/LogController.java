da7c923c4bf667cfafff916a94839c332e88e5f0
xrf416933696 <xrf930301@163.com>
2016-10-18 20:35:33 +0800

    logcontroller

---
+++ src/com/bupt/termdemo/controller/LogController.java
@@ -0,0 +1,10 @@
+package com.bupt.termdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("logController")
+@RequestMapping("/logcontroller")
+public class LogController {
+
+}
--- src/com/bupt/termdemo/controller/TermController.java
+++ src/com/bupt/termdemo/controller/TermController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/TermCtl")
+@RequestMapping("/Term")
 public class TermController {
 
 }
