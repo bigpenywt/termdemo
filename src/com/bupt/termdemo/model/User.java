13eb23b08c36833a16b693719d6029725c9cc83f
xrf416933696 <xrf930301@163.com>
2016-10-23 11:04:49 +0800

    1.

--- src/com/bupt/termdemo/model/User.java
+++ src/com/bupt/termdemo/model/User.java
@@ -4,6 +4,10 @@ public class User {
 	
 	private String username;
 	private String password;
+	private String name;
+	private String pinyin;
+	private String tel;
+	private String email;
 	private String userrole;
 	
 	public String getUsername() {
@@ -24,8 +28,33 @@ public class User {
 	public void setUserrole(String userrole) {
 		this.userrole = userrole;
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPinyin() {
+		return pinyin;
+	}
+	public void setPinyin(String pinyin) {
+		this.pinyin = pinyin;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	@Override
 	public String toString() {
-		return "User [username=" + username + ", password=" + password + ", userrole=" + userrole + "]";
+		return "User [username=" + username + ", password=" + password + ", name=" + name + ", pinyin=" + pinyin
+				+ ", tel=" + tel + ", email=" + email + ", userrole=" + userrole + "]";
 	}
 }
