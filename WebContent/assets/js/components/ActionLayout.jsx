8b148948d4364feb7b891466f491dbc78aee821a
lidad <497718909@qq.com>
2016-10-25 14:45:24 +0800

    modify action list

--- WebContent/assets/js/components/ActionLayout.jsx
+++ WebContent/assets/js/components/ActionLayout.jsx
@@ -10,6 +10,7 @@ export default class ActionLayout extends React.Component {
         this.state = {
             current: ''
         }
+        this.handleClick = this.handleClick.bind(this);
     }
     handleClick(e) {
         this.setState({current: e.key});
@@ -20,20 +21,27 @@ export default class ActionLayout extends React.Component {
             <div>
                 <div className="action-list">
                     <Menu theme={'dark'} onClick={this.handleClick} style={{
-                        width: 240
-                    }} mode="inline">
+                        width: 190
+                    }} selectedKeys={[this.state.current]} mode="inline">
                         <SubMenu key="create" title={< span > <Icon type="mail"/> < span > 新词操作 < /span></span >}>
                             <Menu.Item key="1">创建新词</Menu.Item>
-                            <Menu.Item key="2">待校验单词（我添加的）</Menu.Item>
-                            <Menu.Item key="3">被驳回单词</Menu.Item>
+                            <Menu.Item key="2">待校验的单词（我添加的）</Menu.Item>
+                            <Menu.Item key="3">被驳回的单词</Menu.Item>
                         </SubMenu>
                         <SubMenu key="sub2" title={< span > <Icon type="appstore"/> < span > 校验操作 < /span></span >}>
-                            <Menu.Item key="5">待校验单词（所有的）</Menu.Item>
-                            <Menu.Item key="6">已驳回单词</Menu.Item>
+                            <Menu.Item key="5">待校验的单词（所有的）</Menu.Item>
+                            <Menu.Item key="6">已驳回的单词</Menu.Item>
                         </SubMenu>
-                        <SubMenu key="sub4" title={< span > <Icon type="setting"/> < span > 词库维护 < /span></span >}>
-                            <Menu.Item key="9">待发布单词</Menu.Item>
-                            <Menu.Item key="10">已发布单词</Menu.Item>
+                        <SubMenu key="sub4" title={< span > <Icon type="setting"/> < span > 单词发布 < /span></span >}>
+                            <Menu.Item key="9">待发布的单词</Menu.Item>
+                            <Menu.Item key="10">已驳回的单词</Menu.Item>
+                        </SubMenu>
+                        <SubMenu key="sub5" title={< span > <Icon type="setting"/> < span > 词库维护 < /span></span >}>
+                            <Menu.Item key="11">已发布的单词</Menu.Item>
+                        </SubMenu>
+                        <SubMenu key="sub6" title={< span > <Icon type="setting"/> < span > 系统管理 < /span></span >}>
+                            <Menu.Item key="12">用户管理</Menu.Item>
+                            <Menu.Item key="13">查看日志</Menu.Item>
                         </SubMenu>
                     </Menu>
                 </div>
--- WebContent/webpack.config.js
+++ WebContent/webpack.config.js
@@ -33,20 +33,20 @@ module.exports = {
         }]
     },
     plugins: [
-        // new webpack.DefinePlugin({
-        //     'process.env': {
-        //         'NODE_ENV': JSON.stringify('production')
-        //     }
-        // }),
-        // new webpack.optimize.UglifyJsPlugin({
-        //     minimize: true,
-        //     compress: {
-        //         warnings: false
-        //     },
-        //     output: {
-        //         comments: false,
-        //     },
-        //
-        // })
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            compress: {
+                warnings: false
+            },
+            output: {
+                comments: false,
+            },
+
+        })
     ]
 }
