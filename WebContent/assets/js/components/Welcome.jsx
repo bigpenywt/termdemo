11276bf4d41f722d457bdd16ce0d62fc262e5ec4
lidad <497718909@qq.com>
2016-11-05 18:54:06 +0800

    modify index page

--- WebContent/assets/js/components/Welcome.jsx
+++ WebContent/assets/js/components/Welcome.jsx
@@ -1,32 +1,24 @@
 import React from 'react';
 import {PieChart} from 'react-d3-components';
-import {Card} from 'antd';
 import request from 'superagent';
+import {Card, Form, Input, Row, Col} from 'antd';
+
+const FormItem = Form.Item;
+
+import '../../../public/css/page/app/welcome.css'
 
 export default class Welcome extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             isFirstFetch: true,
-            pieChatdata: {
-                values: [
-                    {
-                        x: '待校验',
-                        y: 10
-                    }, {
-                        x: '被驳回',
-                        y: 4
-                    }, {
-                        x: '待发布',
-                        y: 3
-                    }, {
-                        x: '已发布',
-                        y: 4
-                    }, {
-                        x: '被移除发布',
-                        y: 3
-                    }
-                ]
+            termInfo: {
+                tbModify: 0,
+                deleted: 0,
+                tbPublish: 0,
+                tbReview: 0,
+                done: 0,
+                status: "1"
             }
         }
     }
@@ -34,40 +26,97 @@ export default class Welcome extends React.Component {
         request.get('/termdemo/Term/GetTotal').end((err, res) => {
             let data = JSON.parse(res.text);
             if (data.status === '1') {
-                let pieChatdata = {
-                    values: [
-                        {
-                            x: '待校验',
-                            y: data.tbReview
-                        }, {
-                            x: '被驳回',
-                            y: data.tbModify
-                        }, {
-                            x: '待发布',
-                            y: data.tbPublish
-                        }, {
-                            x: '已发布',
-                            y: data.done
-                        }, {
-                            x: '从「发布」中删除',
-                            y: data.deleted
-                        }
-                    ]
-                }
-                this.setState({pieChatdata: pieChatdata, isFirstFetch: false});
+                this.setState({termInfo: data, isFirstFetch: false});
             }
         });
     }
     render() {
         if (this.state.isFirstFetch)
             return false
-        return (
-            <Card title="当前词库状态" style={{
-                width: '100%'
-            }}>
-                <PieChart data={this.state.pieChatdata} width={550} height={400} sort={null}/>
-            </Card>
-        )
+        else {
+            const pieChatdata = {
+                values: [
+                    {
+                        x: '待校验的单词',
+                        y: this.state.termInfo.tbReview
+                    }, {
+                        x: '驳回的单词',
+                        y: this.state.termInfo.tbModify
+                    }, {
+                        x: '待发布的单词',
+                        y: this.state.termInfo.tbPublish
+                    }, {
+                        x: '已发布的单词',
+                        y: this.state.termInfo.done
+                    }, {
+                        x: '「已发布」中删除的单词',
+                        y: this.state.termInfo.deleted
+                    }
+                ]
+            }
+            return (
+                <Card title="当前词库状态" style={{
+                    width: '100%'
+                }}>
+                    <div style={{
+                        width: '550px'
+                    }}>
+                        <PieChart data={pieChatdata} width={550} height={400} sort={null}/>
+                    </div>
+                    <div className="infoForm">
+                        <Form horizontal>
+                            <Row>
+                                <Col span={24}>
+                                    <FormItem label="待校验" labelCol={{
+                                        span: 16
+                                    }} wrapperCol={{
+                                        span: 8
+                                    }}>
+                                        <p>{this.state.termInfo.tbReview}条</p>
+                                    </FormItem>
+                                </Col>
+                                <Col span={24}>
+                                    <FormItem label="驳回" labelCol={{
+                                        span: 16
+                                    }} wrapperCol={{
+                                        span: 6
+                                    }}>
+                                        <p>{this.state.termInfo.tbModify}条</p>
+                                    </FormItem>
+                                </Col>
+                                <Col span={24}>
+                                    <FormItem label="待发布" labelCol={{
+                                        span: 16
+                                    }} wrapperCol={{
+                                        span: 8
+                                    }}>
+                                        <p>{this.state.termInfo.tbPublish}条</p>
+                                    </FormItem>
+                                </Col>
+                                <Col span={24}>
+                                    <FormItem label="已发布" labelCol={{
+                                        span: 16
+                                    }} wrapperCol={{
+                                        span: 8
+                                    }}>
+                                        <p>{this.state.termInfo.done}条</p>
+                                    </FormItem>
+                                </Col>
+                                <Col span={24}>
+                                    <FormItem label="从「发布」中删除" labelCol={{
+                                        span: 16
+                                    }} wrapperCol={{
+                                        span: 8
+                                    }}>
+                                        <p>{this.state.termInfo.deleted}条</p>
+                                    </FormItem>
+                                </Col>
+                            </Row>
+                        </Form>
+                    </div>
+                </Card>
+            )
+        }
 
     }
 }
