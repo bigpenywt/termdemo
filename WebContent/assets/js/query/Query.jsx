cbc375c7ef4b7b47c3f6e4a2b55196ccd909561e
ywthoho <ywthoho@gmail.com>
2016-11-29 01:40:44 +0800

    窝觉得我们需要一个UI...

--- WebContent/assets/js/query/Query.jsx
+++ WebContent/assets/js/query/Query.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import superagent from 'superagent';
-import { Input, Button } from 'antd';
+import { Input, Button, Row, Col } from 'antd';
+
+import '../../../public/css/page/app/query.css'
 
 function searchTerm(searchTerm) {
   return new Promise((resolve, reject) => {
@@ -49,34 +51,53 @@ class Query extends Component {
   renderResult() {
     const { termData, configuration } = this.state;
     return (
-      <div>
-        <h5>{termData.term}</h5>{this.state.configuration[5] === '1' ? <span>{termData.pronunciation}</span> :null}
-        <div>
-          {this.state.configuration[4] === '1' ? <div>{termData.term_char}</div> : null}
-          {this.state.configuration[1] === '1' ? <div>{termData.definition}</div> : null }
-          {this.state.configuration[2] === '1' ? <div>{termData.source}</div> : null}
-          {this.state.configuration[0] === '1' ? <div>{termData.origin}</div> : null}
-          {this.state.configuration[6] === '1' ? <div>{termData.translation}</div> : null}
-          {this.state.configuration[7] === '1' ? <div>{termData.basis}</div> : null}
-          {this.state.configuration[3] === '1' ? <div>{termData.example}</div> : null}
-        </div>
-      </div>
+      <Row>
+        <Col span={12} offset={2}>
+          <div className="term-panel">
+            <Row type="flex" justify="flex-start">
+              <h2>{termData.term}</h2>
+            </Row>
+            {this.state.configuration[5] === '1' ? <Row><span>{'[' + termData.pronunciation + ']'}</span></Row> :null}
+            <Row>
+              {this.state.configuration[4] === '1' ? <span><strong>{termData.term_char}</strong></span> : null}
+              {this.state.configuration[6] === '1' ? <span>{termData.translation}</span> : null}
+            </Row>
+            <hr/>
+            {this.state.configuration[7] === '1' ? <Row><div>翻译理据</div><div className="indent-content">{termData.basis}</div></Row> : null}
+            <hr />
+            {this.state.configuration[1] === '1' ? <Row><div>英文定义</div><div className="indent-content">{termData.definition}</div></Row> : null }
+            {this.state.configuration[2] === '1' ? <Row><div>定义来源</div><div className="indent-content">{termData.source}</div></Row> : null}
+            <hr />
+            {this.state.configuration[0] === '1' ? <Row><div>首次来源</div><div className="indent-content">{termData.origin}</div></Row> : null}
+            <hr />
+            {this.state.configuration[3] === '1' ? <Row><div>示例</div><div className="indent-content">{termData.example}</div></Row> : null}
+          </div>
+        </Col>
+      </Row>
     )
   }
   render() {
     return (
       this.state.termData.term ?
        <div>
-         <div>
-           <Input value={this.state.searchTerm} onChange={this.handleChange.bind(this)} />
-           <Button type="primary" onClick={this.handleSearch.bind(this)}>查询</Button>
-         </div>
+         <Row className="search-done-header" type="flex" justify="center" align="middle">
+           <Col span={8}><Input value={this.state.searchTerm} onChange={this.handleChange.bind(this)} style={{ width: '95%', height: '40px'}}/></Col>
+           <Col span={2}>
+             <Button type="primary" onClick={this.handleSearch.bind(this)} icon="search" size="large" style={{ width: '105px', marginBottom: '5px'}}>查询</Button>
+           </Col>
+         </Row>
+         <hr style={{ marginTop: '8px', color: '#f1f1f1', marginBottom: '20px' }}/>
          {this.renderResult()}
        </div>
        :
        <div>
-         <Input value={this.state.searchTerm} onChange={this.handleChange.bind(this)} />
-         <Button type="primary" onClick={this.handleSearch.bind(this)}>查询</Button>
+       <Row style={{ height: '25%', position: 'absolute', width: '100%'}} type="flex" justify="center" align="bottom"><h1>蛋蛋术语查询系统</h1></Row>
+       <Row className="initial-search" type="flex" justify="center" align="top">
+         <Col span={8}><Input value={this.state.searchTerm} onChange={this.handleChange.bind(this)} style={{ width: '95%', height: '40px'}}/></Col>
+         <Col span={2}>
+           <Button type="primary" onClick={this.handleSearch.bind(this)} icon="search" size="large" style={{ width: '105px', marginTop: '4px'}}>查询</Button>
+         </Col>
+       </Row>
        </div>
     )
   }
